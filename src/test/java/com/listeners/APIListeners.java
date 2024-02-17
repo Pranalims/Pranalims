47059599266117e975b372d58c43a9889244ec7e
Amar <Amar@gmail.com>
2024-02-17 12:45:28 +0530

    Report Generation Module

--- src/test/java/com/listeners/APIListeners.java
+++ src/test/java/com/listeners/APIListeners.java
@@ -1,13 +1,28 @@
 package com.listeners;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.internal.annotations.ITest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class APIListeners implements ITestListener {
+	private ExtentReports extentReports;
+	private ExtentSparkReporter extentSparkReporter;
+	private ExtentTest extentTest;
 
 	@Override
 	public void onTestStart(ITestResult result) {
+		extentTest = extentReports.createTest(result.getMethod().getMethodName());
 		System.out.println("--------------------- " + result.getTestName() + "--------------------- ");
 		System.out.println("--------------------- " + result.getMethod().getMethodName() + "--------------------- ");
 		System.out.println("--------------------- " + result.getMethod().getDescription() + "---------------------");
@@ -15,18 +30,29 @@ public class APIListeners implements ITestListener {
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
+		extentTest.pass(result.getTestName() + " PASSED!!!!");
+		ITestContext testContext = result.getTestContext();
+		extentTest.log(Status.INFO, (String) testContext.getAttribute("Data"));
+
 		System.out.println("--------------------- " + result.getTestName() + " PASSED!!!! ---------------------");
 
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
+
+		String errorMessage = result.getThrowable().getMessage();
+		extentTest.log(Status.FAIL, errorMessage);
+		extentTest.fail(result.getTestName() + " Failed!!!!");
 		System.out.println("--------------------- " + result.getTestName() + " FAILED!!!! ---------------------");
 
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
+		extentTest.skip(result.getTestName() + " Skipped!!!!!");
+		String errorMessage = result.getThrowable().getMessage();
+		extentTest.log(Status.FAIL, errorMessage);
 		System.out.println("--------------------- " + result.getTestName() + " skipped!!!! ---------------------");
 
 	}
@@ -38,11 +64,40 @@ public class APIListeners implements ITestListener {
 
 	@Override
 	public void onStart(ITestContext context) {
+		extentReports = new ExtentReports();
+		// Code which will create an empty folder called us!!
+		// check if the report is present.... if not presnet create onme
+		// else delete it and then create us
+
+		String reportPath = System.getProperty("user.dir") + "//reports";
+		File f = new File(reportPath); // java io
+		if (f.exists()) {
+			try {
+				FileUtils.forceDelete(f);
+				FileUtils.forceMkdir(f);
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				FileUtils.forceMkdir(f);
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		extentSparkReporter = new ExtentSparkReporter(reportPath);
+		extentReports.attachReporter(extentSparkReporter);
 
 	}
 
 	@Override
 	public void onFinish(ITestContext context) {
+		extentReports.flush();
 
 	}
 
